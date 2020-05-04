a607d8b492932780873c92118870efdbac5bd182
Andrew Loeliger <aloelige@cern.ch>
2020-05-04 07:07:25 -0400

    New 2016 top pt weighting

--- Configurations/UserConfigs/2016/TTConfig.py
+++ Configurations/UserConfigs/2016/TTConfig.py
@@ -8,7 +8,7 @@ from Configurations.Weights.TauFakeRateWeightModule.eTauFakeRateWeight import eT
 from Configurations.Weights.TauIDModule.TauIDWeight import tauIDWeight_2016 as tauIDWeight
 from Configurations.Weights.TriggerSFModule.TriggerWeight import triggerWeight_2016 as triggerWeight
 from Configurations.Weights.bTaggingWeightModule.bTaggingWeight import bTaggingWeight
-from Configurations.Weights.TopPtReweightingModule.TopPtWeight import topPtWeight
+from Configurations.Weights.TopPtReweightingModule.TopPtWeight import topPtWeight_2016
 from Configurations.Weights.PrefiringWeightModule.PrefiringWeight import PrefiringWeighting
 
 from Configurations.ConfigDefinition import ReweightConfiguration
@@ -33,6 +33,6 @@ EWKConfiguration.listOfWeights = [
     tauIDWeight,
     triggerWeight,
     bTaggingWeight,
-    topPtWeight,
+    topPtWeight_2016,
     PrefiringWeighting
 ]
--- Configurations/Weights/TopPtReweightingModule/TopPtWeight.py
+++ Configurations/Weights/TopPtReweightingModule/TopPtWeight.py
@@ -1,6 +1,7 @@
 import ROOT
 import math
 from Configurations.Weights.WeightDefinition import Weight
+from Configurations.Weights import localWeightDataPath
 
 def CalculateTopPtWeight(self,theTree):
     pttop1 = theTree.pt_top1
@@ -10,6 +11,9 @@ def CalculateTopPtWeight(self,theTree):
     if pttop2 > 472:
         pttop2 = 472
     topfactor = math.sqrt(math.exp(0.088-0.00087*pttop1+0.00000092*pttop1*pttop1)*math.exp(0.088-0.00087*pttop2+0.00000092*pttop1*pttop2))
+    if self.year == '2016':
+        correction2016 = (1.0/self.otherCorrection.toppt_ratio_to_2016.Eval(pttop1))*(1.0/self.otherCorrection.toppt_ratio_to_2016.Eval(pttop2))
+        topfactor = topfactor * correction2016
     self.value[0] = topfactor
     
 def CalculateTopPtWeightUp(self,theTree,uncert):
@@ -19,6 +23,7 @@ def CalculateTopPtWeightDown(self,theTree,uncert):
 
 topPtWeight = Weight()
 topPtWeight.name = 'TopPtWeight'
+topPtWeight.year = 'Other'
 topPtWeight.hasUpDownUncertainties = True
 topPtWeight.CalculateWeight = CalculateTopPtWeight
 topPtWeight.uncertaintyVariationList = [
@@ -27,6 +32,22 @@ topPtWeight.uncertaintyVariationList = [
 ]
 topPtWeight.InitUncertaintyVariations()
 topPtWeight.uncertaintyVariationFunctions={
+    "TopPt_UP": CalculateTopPtWeightUp,
+    "TopPt_DOWN": CalculateTopPtWeightDown
+    }
+
+topPtWeight_2016 = Weight()
+topPtWeight_2016.name = 'TopPtWeight'
+topPtWeight_2016.year = '2016'
+topPtWeight_2016.otherCorrection = ROOT.TFile.Open(localWeightDataPath+"toppt_correction_to_2016.root")
+topPtWeight_2016.hasUpDownUncertainties = True
+topPtWeight_2016.CalculateWeight = CalculateTopPtWeight
+topPtWeight_2016.uncertaintyVariationList = [
+    "TopPt_UP",
+    "TopPt_DOWN"
+]
+topPtWeight_2016.InitUncertaintyVariations()
+topPtWeight_2016.uncertaintyVariationFunctions={
     "TopPt_UP": CalculateTopPtWeightUp,
     "TopPt_DOWN": CalculateTopPtWeightDown
     }
