1ccfb9fe521995f2763f850fcc46d8dbdfd09e9a
Andrew Loeliger <aloelige@cern.ch>
2020-10-06 09:50:43 -0400

    Add explicit gen matching to ID functions

--- Configurations/Weights/TauIDModule/TauIDFunctions.py
+++ Configurations/Weights/TauIDModule/TauIDFunctions.py
@@ -8,41 +8,41 @@ def CalculateTauIDWeight(self,theTree):
 def CalculateTauIDWeight_pT0to35_UP(self,theTree,uncert):
     tauVector = ROOT.TLorentzVector()
     tauVector.SetPtEtaPhiM(theTree.pt_2,theTree.eta_2,theTree.phi_2,theTree.m_2)
-    self.uncertaintyVariationArrays[uncert][0] = self.SFTool.getSFvsPT(tauVector.Pt())
+    self.uncertaintyVariationArrays[uncert][0] = self.SFTool.getSFvsPT(tauVector.Pt(),genmatch=theTree.gen_match_2)
     if(tauVector.Pt() < 35.0):
         self.uncertaintyVariationArrays[uncert][0] = self.SFTool.getSFvsPT(tauVector.Pt(),genmatch=theTree.gen_match_2,unc='Up')
 
 def CalculateTauIDWeight_pT0to35_DOWN(self,theTree,uncert):
     tauVector = ROOT.TLorentzVector()
     tauVector.SetPtEtaPhiM(theTree.pt_2,theTree.eta_2,theTree.phi_2,theTree.m_2)
-    self.uncertaintyVariationArrays[uncert][0] = self.SFTool.getSFvsPT(tauVector.Pt())
+    self.uncertaintyVariationArrays[uncert][0] = self.SFTool.getSFvsPT(tauVector.Pt(),genmatch=theTree.gen_match_2)
     if(tauVector.Pt() < 35.0):
         self.uncertaintyVariationArrays[uncert][0] = self.SFTool.getSFvsPT(tauVector.Pt(),genmatch=theTree.gen_match_2,unc='Down')
 
 def CalculateTauIDWeight_pT35to40_UP(self,theTree,uncert):
     tauVector = ROOT.TLorentzVector()
     tauVector.SetPtEtaPhiM(theTree.pt_2,theTree.eta_2,theTree.phi_2,theTree.m_2)
-    self.uncertaintyVariationArrays[uncert][0] = self.SFTool.getSFvsPT(tauVector.Pt())
+    self.uncertaintyVariationArrays[uncert][0] = self.SFTool.getSFvsPT(tauVector.Pt(),genmatch=theTree.gen_match_2)
     if(tauVector.Pt() > 35.0 and tauVector.Pt() < 40.0):
         self.uncertaintyVariationArrays[uncert][0] = self.SFTool.getSFvsPT(tauVector.Pt(),genmatch=theTree.gen_match_2,unc='Up')
 
 def CalculateTauIDWeight_pT35to40_DOWN(self,theTree,uncert):
     tauVector = ROOT.TLorentzVector()
     tauVector.SetPtEtaPhiM(theTree.pt_2,theTree.eta_2,theTree.phi_2,theTree.m_2)
-    self.uncertaintyVariationArrays[uncert][0] = self.SFTool.getSFvsPT(tauVector.Pt())
+    self.uncertaintyVariationArrays[uncert][0] = self.SFTool.getSFvsPT(tauVector.Pt(),genmatch=theTree.gen_match_2)
     if(tauVector.Pt() > 35.0 and tauVector.Pt() < 40.0):
         self.uncertaintyVariationArrays[uncert][0] = self.SFTool.getSFvsPT(tauVector.Pt(),genmatch=theTree.gen_match_2,unc='Down')
 
 def CalculateTauIDWeight_pTgt40_UP(self,theTree,uncert):
     tauVector = ROOT.TLorentzVector()
     tauVector.SetPtEtaPhiM(theTree.pt_2,theTree.eta_2,theTree.phi_2,theTree.m_2)
-    self.uncertaintyVariationArrays[uncert][0] = self.SFTool.getSFvsPT(tauVector.Pt())
+    self.uncertaintyVariationArrays[uncert][0] = self.SFTool.getSFvsPT(tauVector.Pt(),genmatch=theTree.gen_match_2)
     if(tauVector.Pt() > 40.0):
         self.uncertaintyVariationArrays[uncert][0] = self.SFTool.getSFvsPT(tauVector.Pt(),genmatch=theTree.gen_match_2,unc='Up')
 
 def CalculateTauIDWeight_pTgt40_DOWN(self,theTree,uncert):
     tauVector = ROOT.TLorentzVector()
     tauVector.SetPtEtaPhiM(theTree.pt_2,theTree.eta_2,theTree.phi_2,theTree.m_2)
-    self.uncertaintyVariationArrays[uncert][0] = self.SFTool.getSFvsPT(tauVector.Pt())
+    self.uncertaintyVariationArrays[uncert][0] = self.SFTool.getSFvsPT(tauVector.Pt(),genmatch=theTree.gen_match_2)
     if(tauVector.Pt() > 40.0):
         self.uncertaintyVariationArrays[uncert][0] = self.SFTool.getSFvsPT(tauVector.Pt(),genmatch=theTree.gen_match_2,unc='Down')
