963f0a5263378471afbdd8437ddf5346f08149fd
Andrew Loeliger <aloelige@cern.ch>
2020-12-04 04:11:11 -0500

    Add ttH Configurations

---
+++ Configurations/UserConfigs/2016/ttHConfig.py
@@ -0,0 +1,40 @@
+import ROOT 
+
+from Configurations.Weights.CrossSectionWeightingModule.CrossSectionWeight import crossSectionWeight
+from Configurations.Weights.MuIDIsoReweightingModule.MuIDIsoWeight import muIDIsoWeight_2016 as muIDIsoWeight
+from Configurations.Weights.MuTrackingWeightModule.MuTrackingWeight import muTrackingWeight_2016 as muTrackingWeight
+from Configurations.Weights.PileupWeightingModule.PileupWeight import pileupWeight_2016 as pileupWeight
+from Configurations.Weights.TauFakeRateWeightModule.TauFakeRateWeight import tauFakeRateWeight_2016 as tauFakeRateWeight
+from Configurations.Weights.TauIDModule.TauIDWeight import tauIDWeight_2016 as tauIDWeight
+from Configurations.Weights.TriggerSFModule.TriggerWeight import triggerWeight_2016 as triggerWeight
+from Configurations.Weights.bTaggingWeightModule.bTaggingWeight import bTaggingWeight_2016
+from Configurations.Weights.PrefiringWeightModule.PrefiringWeight import PrefiringWeighting
+from Configurations.Weights.TopPtReweightingModule.TopPtWeight import topPtWeight_2016
+from Configurations.Weights.QCDAcceptanceWeights.DifferentialQCDAcceptanceWeights.DifferentialQCDAcceptanceWeight import ttHStyleWeight_Differential
+
+from Configurations.ConfigDefinition import ReweightConfiguration
+
+EWKConfiguration = ReweightConfiguration()
+EWKConfiguration.name = "ttHtoNonbb"
+EWKConfiguration.inputFile = "/data/aloeliger/SMHTT_Selected_2016_Deep/ttH.root"
+crossSectionWeight.sample = 'ttHtoNonbb'
+crossSectionWeight.year = '2016'
+totalEventsFile = ROOT.TFile.Open("/data/aloeliger/SMHTT_Selected_2016_Deep/VBF.root")
+crossSectionWeight.totalEvents = totalEventsFile.eventCount.GetBinContent(2)
+totalEventsFile.Close()
+pileupWeight.year = '2016'
+pileupWeight.sample = 'ttHtoNonbb'
+pileupWeight.InitPileupWeightings(pileupWeight)
+EWKConfiguration.listOfWeights = [
+    crossSectionWeight,
+    muIDIsoWeight,
+    muTrackingWeight,
+    pileupWeight,
+    tauFakeRateWeight,
+    tauIDWeight,
+    triggerWeight,
+    bTaggingWeight_2016,    
+    topPtWeight_2016,
+    PrefiringWeighting,
+    ttHStyleWeight_Differential,
+]
---
+++ Configurations/UserConfigs/2018/ttHConfig.py
@@ -0,0 +1,38 @@
+import ROOT 
+
+from Configurations.Weights.CrossSectionWeightingModule.CrossSectionWeight import crossSectionWeight
+from Configurations.Weights.MuIDIsoReweightingModule.MuIDIsoWeight import muIDIsoWeight_2018 as muIDIsoWeight
+from Configurations.Weights.MuTrackingWeightModule.MuTrackingWeight import muTrackingWeight_2018 as muTrackingWeight
+from Configurations.Weights.PileupWeightingModule.PileupWeight import pileupWeight_2018 as pileupWeight
+from Configurations.Weights.TauFakeRateWeightModule.TauFakeRateWeight import tauFakeRateWeight_2018 as tauFakeRateWeight
+from Configurations.Weights.TauIDModule.TauIDWeight import tauIDWeight_2018 as tauIDWeight
+from Configurations.Weights.TriggerSFModule.TriggerWeight import triggerWeight_2018 as triggerWeight
+from Configurations.Weights.bTaggingWeightModule.bTaggingWeight import bTaggingWeight_2018
+from Configurations.Weights.TopPtReweightingModule.TopPtWeight import topPtWeight
+from Configurations.Weights.QCDAcceptanceWeights.DifferentialQCDAcceptanceWeights.DifferentialQCDAcceptanceWeight import ttHStyleWeight_Differential
+
+from Configurations.ConfigDefinition import ReweightConfiguration
+
+EWKConfiguration = ReweightConfiguration()
+EWKConfiguration.name = "ttHtoTauTau"
+EWKConfiguration.inputFile = "/data/aloeliger/SMHTT_Selected_2018_Deep/ttH.root"
+crossSectionWeight.sample = 'ttHtoTauTau'
+crossSectionWeight.year = '2018'
+totalEventsFile = ROOT.TFile.Open("/data/aloeliger/SMHTT_Selected_2018_Deep/VBF.root")
+crossSectionWeight.totalEvents = totalEventsFile.eventCount.GetBinContent(2)
+totalEventsFile.Close()
+pileupWeight.year = '2018'
+pileupWeight.sample = 'ttHtoNonbb'
+pileupWeight.InitPileupWeightings(pileupWeight)
+EWKConfiguration.listOfWeights = [
+    crossSectionWeight,
+    muIDIsoWeight,
+    muTrackingWeight,
+    pileupWeight,
+    tauFakeRateWeight,
+    tauIDWeight,
+    triggerWeight,
+    bTaggingWeight_2018,    
+    topPtWeight,
+    ttHStyleWeight_Differential,
+]
