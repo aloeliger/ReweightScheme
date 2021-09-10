202ba77e7b6ef32e414525fad4db674538886964
Andrew David Loeliger <andrew.loeliger@cern.ch>
2021-09-10 16:14:12 -0400

    Add NanoAOD pileup weight files

--- Configurations/Weights/__init__.py
+++ Configurations/Weights/__init__.py
@@ -1,5 +1,6 @@
-#quick initialization to help us find our weights
+0#quick initialization to help us find our weights
 import os
 localWeightPath, initFile = os.path.split(os.path.realpath(__file__))
 localWeightDataPath = localWeightPath+"/Data/"
+b2gWeightPath = localWeightDataPath+'/b2gWeightFiles/'
 print("Reading weights from: "+localWeightDataPath)
